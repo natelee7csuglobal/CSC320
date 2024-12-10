9b3ec16387b42f6c84fa3d2e913dffda49a9173a
natelee7csuglobal <nathanial.anderson@csuglobal.edu>
2024-12-10 10:05:11 -0600

    Module 1 File Upload

---
+++ printName.java
@@ -0,0 +1,40 @@
+package module1;
+
+
+// Module 1 CSC320
+// Nate Anderson
+// 12/10/2024
+// Option 1
+// Professor: Herbert Pensado
+
+// Pseudocode
+// Develop a program that prints out a name and address
+// Declare a variable for First Name
+// Declare a variable for Last Name
+// Declare a variable for Street Address
+// Declare a variable for City
+// Declare a variable for Zip Code
+// Print each of the variables on their own line
+
+public class printName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declare all variables
+		String firstName = "Nate";
+		String lastName = "Anderson";
+		String streetAddress = "555 NW Spring St";
+		String city = "Evergreen";
+		String zipCode = "55555";
+		
+		// Print Statements
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + streetAddress);
+		System.out.println("City: " + city);
+		System.out.println("Zip Code: " + zipCode);
+
+	}
+
+}
